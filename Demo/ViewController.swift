602a49b0d261e1ef42a7bb85b19da39a06b4752a
Jun <zaczh@users.noreply.github.com>
2016-12-21 11:18:20 +0800

    Update ViewController.swift

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -13,7 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        AClass.afoo{ (data, error) -> Void in
+        super.viewDidLoad()
+        
+        let orderby = "time"
+        let url = "http://www.sample.com/api/index.php?\(orderby)"
+        
+        Log("Request url: \(url)")
+        
+        if let url = URL(string: url) {
+            let request = URLRequest(url: url)
+            print(request)
         }
     }
 
@@ -23,25 +32,6 @@ class ViewController: UIViewController {
     }
 
 
-}
-
-class AClass: NSObject {
-    class func afoo(_ completion: ((AnyObject?, NSError?) -> Void)) {
-        let page = 1
-        let fid = "75"
-        let orderby = "time"
-        let url = "test.html?&\(page)&fid=\(fid)&orderby=\(orderby)"
-        
-        //if comment out this line, then everything is OK.
-        Log("requestTopicListOfForum url: \(url)")
-        
-        let session = URLSession.shared
-        let request = URLRequest(url: URL(string: url, relativeTo: URL(string: "https://www.example.com/")!)!)
-        let task = session.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
-        })
-        
-        task.resume()
-    }
 }
 
 func Log(_ log: String) {
